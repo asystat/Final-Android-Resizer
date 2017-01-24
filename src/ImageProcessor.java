c1663f019c83132e4a40da681a23950470a3f870
Lawrence Pickford <lawrence.pickford@gastag.co.uk>
2017-01-24 17:01:58 +0000

    Implemented Xamarin (iOS and Windows) folder/file support

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -19,106 +19,149 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
  * */
 
 
+import com.mortennobel.imagescaling.AdvancedResizeOp;
+import com.mortennobel.imagescaling.ResampleFilters;
+import com.mortennobel.imagescaling.ResampleOp;
+import com.sun.istack.internal.NotNull;
+
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
-import com.mortennobel.imagescaling.AdvancedResizeOp;
-import com.mortennobel.imagescaling.ResampleFilters;
-import com.mortennobel.imagescaling.ResampleOp;
-
 public class ImageProcessor {
-	
-	private static final float LDPI_RATIO=3;
-	private static final float MDPI_RATIO=4;
-	private static final float TVDPI_RATIO=5.33333333f;
-	private static final float HDPI_RATIO=6;
-	private static final float XHDPI_RATIO=8;
-	private static final float XXHDPI_RATIO=12;
-	private static final float XXXHDPI_RATIO=16;
-	
 
-	public static void processImage(File f, File resDirectory, String originalSize, String drawableDirectory, boolean overwrite,
-			String resFolder) throws FileAlreadyExistsException, IOException, NullPointerException {
+    private static final float LDPI_RATIO = 3;
+    private static final float MDPI_RATIO = 4;
+    private static final float TVDPI_RATIO = 5.33333333f;
+    private static final float HDPI_RATIO = 6;
+    private static final float XHDPI_RATIO = 8;
+    private static final float XXHDPI_RATIO = 12;
+    private static final float XXXHDPI_RATIO = 16;
+    private static final float X1_RATIO = 4;
+    private static final float X15_RATIO = 6;
+    private static final float X2_RATIO = 8;
+    private static final float X3_RATIO = 12;
+    private static final float X4_RATIO = 16;
+    private static final float WINDOWS_RATIO = 12;
 
-		String finalPath = "";
 
-		if(drawableDirectory.equalsIgnoreCase("mipmap")) {
-			finalPath = resDirectory.getAbsolutePath() + "/res-mipmap/mipmap-" + resFolder + "/" + f.getName();
-		}else{
-			finalPath = resDirectory.getAbsolutePath() + "/res-drawable/drawable-" + resFolder + "/" + f.getName();
-		}
-		
-		File destFile=new File(finalPath);
-		if(!overwrite) {
-			if (destFile.exists())
-				throw new FileAlreadyExistsException();
-		}
-		
-		destFile.getParentFile().mkdirs();
-		
-		BufferedImage image = ImageIO.read(f);
-		
-		int size=getRequiredSize(originalSize,resFolder,image.getWidth());
+    public static void processImage(File f, File resDirectory, String originalSize, String drawableDirectory, boolean overwrite,
+                                    String resFolder, @NotNull DrawableType type) throws FileAlreadyExistsException, IOException, NullPointerException {
 
-		ResampleOp resampleOp = new ResampleOp(size, (size * image.getHeight())
-				/ image.getWidth());
-		resampleOp.setFilter(ResampleFilters.getLanczos3Filter());
-		resampleOp.setUnsharpenMask(AdvancedResizeOp.UnsharpenMask.None);
-		image = resampleOp.filter(image, null);
+        String finalPath = "";
 
-		ImageIO.write(image, getExtension(f.getName()), destFile);
-	}
+        switch (type) {
+            case ANDROID:
+                if (drawableDirectory.equalsIgnoreCase("mipmap")) {
+                    finalPath = resDirectory.getAbsolutePath() + "/res-mipmap/mipmap-" + resFolder + "/" + f.getName();
+                } else {
+                    finalPath = resDirectory.getAbsolutePath() + "/res-drawable/drawable-" + resFolder + "/" + f.getName();
+                }
+                break;
+            case IOS:
+                finalPath = resDirectory.getAbsolutePath() + "/" + stripExtension(f.getName()) + resFolder + "." + getExtension(f.getName());
+                break;
+            case WINDOWS:
+                finalPath = resDirectory.getAbsolutePath() + "/" + f.getName();
+                break;
+        }
 
-	private static int getRequiredSize(String originalSize, String resFolder,
-			int width) {
-		float destRatio=1;
-		if(resFolder.equalsIgnoreCase("ldpi")) {
-			destRatio = LDPI_RATIO;
-		}else if(resFolder.equalsIgnoreCase("mdpi")) {
-			destRatio = MDPI_RATIO;
-		}else if(resFolder.equalsIgnoreCase("tvdpi")) {
-			destRatio = TVDPI_RATIO;
-		}else if(resFolder.equalsIgnoreCase("hdpi")) {
-			destRatio = HDPI_RATIO;
-		}else if(resFolder.equalsIgnoreCase("xhdpi")) {
-			destRatio = XHDPI_RATIO;
-		}else if(resFolder.equalsIgnoreCase("xxhdpi")) {
-			destRatio = XXHDPI_RATIO;
-		}else if(resFolder.equalsIgnoreCase("xxxhdpi")) {
-			destRatio = XXXHDPI_RATIO;
-		}
 
-		float origRatio=1;
-		if(originalSize.equalsIgnoreCase("ldpi")) {
-			origRatio = LDPI_RATIO;
-		}else if(originalSize.equalsIgnoreCase("mdpi")) {
-			origRatio = MDPI_RATIO;
-		}else if(originalSize.equalsIgnoreCase("tvdpi")) {
-			origRatio = TVDPI_RATIO;
-		}else if(originalSize.equalsIgnoreCase("hdpi")) {
-			origRatio = HDPI_RATIO;
-		}else if(originalSize.equalsIgnoreCase("xhdpi")) {
-			origRatio = XHDPI_RATIO;
-		}else if(originalSize.equalsIgnoreCase("xxhdpi")) {
-			origRatio = XXHDPI_RATIO;
-		}else if(originalSize.equalsIgnoreCase("xxxhdpi")) {
-			origRatio = XXXHDPI_RATIO;
-		}
+        File destFile = new File(finalPath);
+        if (!overwrite) {
+            if (destFile.exists())
+                throw new FileAlreadyExistsException();
+        }
 
-		return Math.round(((float)width)*destRatio/origRatio);
-	}
+        destFile.getParentFile().mkdirs();
 
-	private static String getExtension(String f) {
-		String extension = "";
+        BufferedImage image = ImageIO.read(f);
 
-		int i = f.lastIndexOf('.');
-		if (i > 0) {
-		    extension = f.substring(i+1);
-		}
-		return extension;
-	}
+        int size = getRequiredSize(originalSize, resFolder, image.getWidth());
 
+        ResampleOp resampleOp = new ResampleOp(size, (size * image.getHeight())
+                / image.getWidth());
+        resampleOp.setFilter(ResampleFilters.getLanczos3Filter());
+        resampleOp.setUnsharpenMask(AdvancedResizeOp.UnsharpenMask.None);
+        image = resampleOp.filter(image, null);
+
+        ImageIO.write(image, getExtension(f.getName()), destFile);
+    }
+
+    private static int getRequiredSize(String originalSize, String resFolder,
+                                       int width) {
+        float destRatio = 1;
+        destRatio = getRatio(resFolder);
+
+        float origRatio = 1;
+        origRatio = getRatio(originalSize);
+
+        return Math.round(((float) width) * destRatio / origRatio);
+    }
+
+    private static float getRatio(String ratioString) {
+        if (ratioString.equalsIgnoreCase("ldpi")) {
+            return LDPI_RATIO;
+        } else if (ratioString.equalsIgnoreCase("mdpi")) {
+            return MDPI_RATIO;
+        } else if (ratioString.equalsIgnoreCase("tvdpi")) {
+            return TVDPI_RATIO;
+        } else if (ratioString.equalsIgnoreCase("hdpi")) {
+            return HDPI_RATIO;
+        } else if (ratioString.equalsIgnoreCase("xhdpi")) {
+            return XHDPI_RATIO;
+        } else if (ratioString.equalsIgnoreCase("xxhdpi")) {
+            return XXHDPI_RATIO;
+        } else if (ratioString.equalsIgnoreCase("xxxhdpi")) {
+            return XXXHDPI_RATIO;
+        } else if (ratioString.equalsIgnoreCase("@1x")) {
+            return X1_RATIO;
+        } else if (ratioString.equalsIgnoreCase("@1.5x")) {
+            return X15_RATIO;
+        } else if (ratioString.equalsIgnoreCase("@2x")) {
+            return X2_RATIO;
+        } else if (ratioString.equalsIgnoreCase("@3x")) {
+            return X3_RATIO;
+        } else if (ratioString.equalsIgnoreCase("@4x")) {
+            return X4_RATIO;
+        } else if (ratioString.equalsIgnoreCase("windows")) {
+            return WINDOWS_RATIO;
+        }
+        return 1;
+    }
+
+    private static String getExtension(String f) {
+        String extension = "";
+
+        int i = f.lastIndexOf('.');
+        if (i > 0) {
+            extension = f.substring(i + 1);
+        }
+        return extension;
+    }
+
+    public enum DrawableType {
+        ANDROID,
+        IOS,
+        WINDOWS
+    }
+
+    static String stripExtension(String str) {
+        // Handle null case specially.
+
+        if (str == null) return null;
+
+        // Get position of last '.'.
+
+        int pos = str.lastIndexOf(".");
+
+        // If there wasn't any '.' just return the string as is.
+
+        if (pos == -1) return str;
+
+        // Otherwise return the string, up to the dot.
+
+        return str.substring(0, pos);
+    }
 }
